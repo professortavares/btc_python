99e51cfbb3d15dc6caed8fb7d506c45c4018d60b
Prof. Tavares <leodaniel@gmail.com>
2022-03-29 08:34:17 -0300

    definição dos métodos para TODO LIST

--- main.py
+++ main.py
@@ -7,3 +7,24 @@ app = FastAPI()
 @app.get("/")
 def root(nome:str = "world!!!"):
     return {"message": f"Hello {nome}"}
+
+
+@app.post("/todo")
+def criar_tarefa():
+    pass
+
+@app.get("/todo/{id}")
+def ler_tarefa_por_id(id:int):
+    pass
+
+@app.put("/todo/{id}")
+def atualizar_tarefa_por_id(id:int):
+    pass
+
+@app.delete("/todo/{id}")
+def excluir_tarefa_por_id(id:int):
+    pass
+
+@app.get("/todo")
+def ler_todas_tarefas():
+    pass
